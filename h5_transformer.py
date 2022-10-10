e3f7b15ab84b7d96735e89bdfebb9758f8735fe3
xjh <xjh19972@gmail.com>
2022-10-10 18:38:00 -0400

    update yml and h5 transformer

---
+++ h5_transformer.py
@@ -0,0 +1,41 @@
+import h5py
+import numpy as np
+import os
+from PIL import Image
+import argparse
+from tqdm import tqdm
+
+ALLOWED_EXT = ['jpeg', 'jpg', 'png']
+parser = argparse.ArgumentParser()
+parser.add_argument('--read_path', type=str, help='The path of the folder that contains jpg or png images')
+parser.add_argument('--save_path', type=str, help='The path of the folder that contains jpg or png images')
+parser.add_argument('--resize_height', type=int, default=480)
+parser.add_argument('--resize_width', type=int, default=640)
+args = parser.parse_args()
+
+# To create h5 file, we must resize the images
+if os.path.isfile(args.save_path):
+    os.remove(args.save_path)
+hf = h5py.File(args.save_path, 'a') # open a hdf5 file
+start = False
+for img_name in tqdm(os.listdir(args.read_path)):
+    extension = img_name.split('.')[-1]
+    if extension in ALLOWED_EXT:
+        img = Image.open(os.path.join(args.read_path, img_name)).convert("RGB")
+        new_img = img.resize((args.resize_width, args.resize_height))
+        img_np = np.array(new_img)
+        img_np = np.expand_dims(img_np, axis = 0)
+        size_np = np.expand_dims(np.array([img.height, img.width]), axis=0)
+        if not start:
+            hf.create_dataset('image_data', data=img_np, chunks=True, maxshape=(None, 640, 640, 3))  # write the data to hdf5 file
+            hf.create_dataset('image_size', data=size_np, chunks=True, maxshape=(None, 2))
+            start = True
+        else:
+            hf['image_data'].resize(hf['image_data'].shape[0] + img_np.shape[0], axis=0)
+            hf['image_data'][-img_np.shape[0]:] = img_np
+            hf['image_size'].resize(hf['image_size'].shape[0] + size_np.shape[0], axis=0)
+            hf['image_size'][-size_np.shape[0]:] = size_np
+    else:
+        continue
+hf.close()  # close the hdf5 file
+print('hdf5 file size: %d bytes'%os.path.getsize(args.save_path))
---
+++ transform_dataset.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+eval "$(conda shell.bash hook)"
+conda activate VPR_SSL
+
+# eynsham
+python h5_transformer.py --read_path ~/datasets_vg/datasets/eynsham/images/test/database --save_path  ~/datasets_vg/datasets/eynsham/database.h5 &
+python h5_transformer.py --read_path ~/datasets_vg/datasets/eynsham/images/test/queries --save_path  ~/datasets_vg/datasets/eynsham/query.h5 &
+
+# msls
+python h5_transformer.py --read_path ~/datasets_vg/datasets/msls/train/database --save_path  ~/datasets_vg/datasets/msls/train_database.h5 &
+python h5_transformer.py --read_path ~/datasets_vg/datasets/msls/train/queries --save_path  ~/datasets_vg/datasets/msls/train_queries.h5 &
+python h5_transformer.py --read_path ~/datasets_vg/datasets/msls/val/database --save_path  ~/datasets_vg/datasets/msls/val_database.h5 &
+python h5_transformer.py --read_path ~/datasets_vg/datasets/msls/val/queries --save_path  ~/datasets_vg/datasets/msls/val_queries.h5 &
+
+# nordland
+python h5_transformer.py --read_path ~/datasets_vg/datasets/nordland/images/test/database --save_path  ~/datasets_vg/datasets/nordland/database.h5 &
+python h5_transformer.py --read_path ~/datasets_vg/datasets/nordland/images/test/queries --save_path  ~/datasets_vg/datasets/nordland/queries.h5 &
+
+# pitts30k
+python h5_transformer.py --read_path ~/datasets_vg/datasets/pitts30k/images/test/database --save_path  ~/datasets_vg/datasets/pitts30k/test_database.h5 &
+python h5_transformer.py --read_path ~/datasets_vg/datasets/pitts30k/images/test/queries --save_path  ~/datasets_vg/datasets/pitts30k/test_queries.h5 &
+python h5_transformer.py --read_path ~/datasets_vg/datasets/pitts30k/images/train/database --save_path  ~/datasets_vg/datasets/pitts30k/train_database.h5 &
+python h5_transformer.py --read_path ~/datasets_vg/datasets/pitts30k/images/train/queries --save_path  ~/datasets_vg/datasets/pitts30k/train_queries.h5 &
+python h5_transformer.py --read_path ~/datasets_vg/datasets/pitts30k/images/val/database --save_path  ~/datasets_vg/datasets/pitts30k/val_database.h5 &
+python h5_transformer.py --read_path ~/datasets_vg/datasets/pitts30k/images/val/queries --save_path  ~/datasets_vg/datasets/pitts30k/val_queries.h5 &
+
+# pitts250k
+python h5_transformer.py --read_path ~/datasets_vg/datasets/pitts250k/images/test/database --save_path  ~/datasets_vg/datasets/pitts250k/test_database.h5 &
+python h5_transformer.py --read_path ~/datasets_vg/datasets/pitts250k/images/test/queries --save_path  ~/datasets_vg/datasets/pitts250k/test_queries.h5 &
+python h5_transformer.py --read_path ~/datasets_vg/datasets/pitts250k/images/train/database --save_path  ~/datasets_vg/datasets/pitts250k/train_database.h5 &
+python h5_transformer.py --read_path ~/datasets_vg/datasets/pitts250k/images/train/queries --save_path  ~/datasets_vg/datasets/pitts250k/train_queries.h5 &
+python h5_transformer.py --read_path ~/datasets_vg/datasets/pitts250k/images/val/database --save_path  ~/datasets_vg/datasets/pitts250k/val_database.h5 &
+python h5_transformer.py --read_path ~/datasets_vg/datasets/pitts250k/images/val/queries --save_path  ~/datasets_vg/datasets/pitts250k/val_queries.h5 &
+
+#san_francisco
+python h5_transformer.py --read_path ~/datasets_vg/datasets/san_francisco/images/test/database --save_path  ~/datasets_vg/datasets/san_francisco/database.h5 &
+python h5_transformer.py --read_path ~/datasets_vg/datasets/san_francisco/images/test/queries --save_path  ~/datasets_vg/datasets/san_francisco/queries.h5 &
+
+#san_francisco
+python h5_transformer.py --read_path ~/datasets_vg/datasets/st_lucia/images/test/database --save_path  ~/datasets_vg/datasets/st_lucia/database.h5 &
+python h5_transformer.py --read_path ~/datasets_vg/datasets/st_lucia/images/test/queries --save_path  ~/datasets_vg/datasets/st_lucia/queries.h5 &
+
+#tokyo247
+python h5_transformer.py --read_path ~/datasets_vg/datasets/tokyo247/images/test/database --save_path  ~/datasets_vg/datasets/tokyo247/database.h5 &
+python h5_transformer.py --read_path ~/datasets_vg/datasets/tokyo247/images/test/queries --save_path  ~/datasets_vg/datasets/tokyo247/queries.h5 &
+
