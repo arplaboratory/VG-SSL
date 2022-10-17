6181920326a2026e35585e6d427ae079ed81170b
Jiuhong Xiao <jx1190@log-3.hpc.nyu.edu>
2022-10-17 12:05:02 -0400

    update test script

--- test_dataset_h5_availibility.sh
+++ test_dataset_h5_availibility.sh
@@ -3,26 +3,26 @@ eval "$(conda shell.bash hook)"
 conda activate VPR_SSL
 
 # eynsham
-python h5_reader.py --read_path ~/datasets_vg/datasets/eynsham/
+python h5_reader.py --read_path ./datasets/eynsham/
 
 # msls
-python h5_reader.py --read_path ~/datasets_vg/datasets/msls/
+python h5_reader.py --read_path ./datasets/msls/
 
 # nordland
-python h5_reader.py --read_path ~/datasets_vg/datasets/nordland
+python h5_reader.py --read_path ./datasets/nordland
 
 # # pitts30k
-python h5_reader.py --read_path ~/datasets_vg/datasets/pitts30k
+python h5_reader.py --read_path ./datasets/pitts30k
 
 # # pitts250k
-python h5_reader.py --read_path ~/datasets_vg/datasets/pitts250k
+python h5_reader.py --read_path ./datasets/pitts250k
 
 # #san_francisco
-python h5_reader.py --read_path ~/datasets_vg/datasets/san_francisco
+python h5_reader.py --read_path ./datasets/san_francisco
 
 # #st_lucia
-python h5_reader.py --read_path ~/datasets_vg/datasets/st_lucia/
+python h5_reader.py --read_path ./datasets/st_lucia/
 
 # #tokyo247
-python h5_reader.py --read_path ~/datasets_vg/datasets/tokyo247
+python h5_reader.py --read_path ./datasets/tokyo247
 
