5d3cc2f29dab0559c22813a80c0f04df16106518
xjh <xjh19972@gmail.com>
2022-10-18 13:16:00 -0400

    update reader for size

--- h5_reader.py
+++ h5_reader.py
@@ -13,11 +13,16 @@ args = parser.parse_args()
 for h5_file_name in os.listdir(args.read_path):
     extension = h5_file_name.split('.')[-1]
     if extension == 'h5':
-        hf = h5py.File(os.path.join(args.read_path, h5_file_name), 'r') # open a hdf5 file
-        print(os.path.join(args.read_path, h5_file_name))
-        print(hf['image_name'][0])
-        print(hf['image_size'][0])
-        print(hf['image_data'].shape)
-        print(hf['image_data'][0,0:10,0:10,0])
         print('====================================')
-        hf.close()  # close the hdf5 file
+        try:
+            hf = h5py.File(os.path.join(args.read_path, h5_file_name), 'r') # open a hdf5 file
+            print(os.path.join(args.read_path, h5_file_name))
+            print(hf['image_name'][0])
+            print(hf['image_size'][0])
+            print(hf['image_data'].shape)
+            print(hf['image_data'][0,0:10,0:10,0])
+            hf.close()  # close the hdf5 file
+        except Exception as e:
+            print(f'Reading error for {args.read_path}')
+            print(f'{e}')
+        print('====================================')
