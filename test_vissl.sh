1b009d2303e2ebefdd0c2d671f5d3a9cd25dcfc3
Jiuhong Xiao <jx1190@log-2.hpc.nyu.edu>
2022-11-02 23:43:36 -0400

    test vissl

--- test_vissl.sh
+++ test_vissl.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sbatch ./script/train_pitts30k_full.sbatch
+eval "$(conda shell.bash hook)"
+conda activate VPR_SSL
+
+python3 run_distributed_engines.py config=quick_1gpu_resnet50_simclr config.DATA.TRAIN.DATA_SOURCES=[synthetic]
