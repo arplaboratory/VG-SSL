19883f93a9213762c55e6fec5605f0125b7c8399
xjh <xjh19972@gmail.com>
2022-11-21 12:40:56 -0500

    update comment

--- train.py
+++ train.py
@@ -196,7 +196,7 @@ for epoch_num in range(start_epoch_num, args.epochs_num):
         torch.cuda.empty_cache()
         model = model.train()
 
-        # images shape: (train_batch_size*12)*3*H*W ; by default train_batch_size=4, H=512, W=512
+        # images shape: (train_batch_size*12)*3*H*W ; by default train_batch_size=4, H=480, W=640
         # triplets_local_indexes shape: (train_batch_size*10)*3 ; because 10 triplets per query
         for images, triplets_local_indexes, _ in tqdm(triplets_dl, ncols=100):
 
