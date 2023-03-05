442b3261a79f99e11aaf7c4c9ae17ea3cf677af6
Jiuhong Xiao <jx1190@log-3.hpc.nyu.edu>
2023-03-05 17:32:02 -0500

    run sbatch

--- train_pitts30k_partial_sbatch.sh
+++ train_pitts30k_partial_sbatch.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # PRETRAIN
-sbatch ./script/train_pitts30k_partial.sbatch --export=ALL,PRETRAIN=simclr,SEED=0
-sbatch ./script/train_pitts30k_partial.sbatch --export=ALL,PRETRAIN=simclr,SEED=1
-sbatch ./script/train_pitts30k_partial.sbatch --export=ALL,PRETRAIN=simclr,SEED=2
-sbatch ./script/train_pitts30k_partial.sbatch --export=ALL,PRETRAIN=moco,SEED=0
-sbatch ./script/train_pitts30k_partial.sbatch --export=ALL,PRETRAIN=moco,SEED=1
-sbatch ./script/train_pitts30k_partial.sbatch --export=ALL,PRETRAIN=moco,SEED=2
-sbatch ./script/train_pitts30k_partial.sbatch --export=ALL,PRETRAIN=mocov2,SEED=0
-sbatch ./script/train_pitts30k_partial.sbatch --export=ALL,PRETRAIN=mocov2,SEED=1
-sbatch ./script/train_pitts30k_partial.sbatch --export=ALL,PRETRAIN=mocov2,SEED=2
-sbatch ./script/train_pitts30k_partial.sbatch --export=ALL,PRETRAIN=byol,SEED=0
-sbatch ./script/train_pitts30k_partial.sbatch --export=ALL,PRETRAIN=byol,SEED=1
-sbatch ./script/train_pitts30k_partial.sbatch --export=ALL,PRETRAIN=byol,SEED=2
-sbatch ./script/train_pitts30k_partial.sbatch --export=ALL,PRETRAIN=vicreg,SEED=0
-sbatch ./script/train_pitts30k_partial.sbatch --export=ALL,PRETRAIN=vicreg,SEED=1
-sbatch ./script/train_pitts30k_partial.sbatch --export=ALL,PRETRAIN=vicreg,SEED=2
-sbatch ./script/train_pitts30k_partial.sbatch --export=ALL,PRETRAIN=swav,SEED=0
-sbatch ./script/train_pitts30k_partial.sbatch --export=ALL,PRETRAIN=swav,SEED=1
-sbatch ./script/train_pitts30k_partial.sbatch --export=ALL,PRETRAIN=swav,SEED=2
-sbatch ./script/train_pitts30k_partial.sbatch --export=ALL,PRETRAIN=bt,SEED=0
-sbatch ./script/train_pitts30k_partial.sbatch --export=ALL,PRETRAIN=bt,SEED=1
-sbatch ./script/train_pitts30k_partial.sbatch --export=ALL,PRETRAIN=bt,SEED=2
+sbatch --export=ALL,PRETRAIN=simclr,SEED=0 ./script/train_pitts30k_partial.sbatch 
+sbatch --export=ALL,PRETRAIN=simclr,SEED=1 ./script/train_pitts30k_partial.sbatch 
+sbatch --export=ALL,PRETRAIN=simclr,SEED=2 ./script/train_pitts30k_partial.sbatch 
+sbatch --export=ALL,PRETRAIN=moco,SEED=0 ./script/train_pitts30k_partial.sbatch 
+sbatch --export=ALL,PRETRAIN=moco,SEED=1 ./script/train_pitts30k_partial.sbatch 
+sbatch --export=ALL,PRETRAIN=moco,SEED=2 ./script/train_pitts30k_partial.sbatch 
+sbatch --export=ALL,PRETRAIN=mocov2,SEED=0 ./script/train_pitts30k_partial.sbatch 
+sbatch --export=ALL,PRETRAIN=mocov2,SEED=1 ./script/train_pitts30k_partial.sbatch 
+sbatch --export=ALL,PRETRAIN=mocov2,SEED=2 ./script/train_pitts30k_partial.sbatch 
+sbatch --export=ALL,PRETRAIN=byol,SEED=0 ./script/train_pitts30k_partial.sbatch 
+sbatch --export=ALL,PRETRAIN=byol,SEED=1 ./script/train_pitts30k_partial.sbatch
+sbatch --export=ALL,PRETRAIN=byol,SEED=2 ./script/train_pitts30k_partial.sbatch 
+sbatch --export=ALL,PRETRAIN=vicreg,SEED=0 ./script/train_pitts30k_partial.sbatch 
+sbatch --export=ALL,PRETRAIN=vicreg,SEED=1 ./script/train_pitts30k_partial.sbatch 
+sbatch --export=ALL,PRETRAIN=vicreg,SEED=2 ./script/train_pitts30k_partial.sbatch 
+sbatch --export=ALL,PRETRAIN=swav,SEED=0 ./script/train_pitts30k_partial.sbatch 
+sbatch --export=ALL,PRETRAIN=swav,SEED=1 ./script/train_pitts30k_partial.sbatch 
+sbatch --export=ALL,PRETRAIN=swav,SEED=2 ./script/train_pitts30k_partial.sbatch 
+sbatch --export=ALL,PRETRAIN=bt,SEED=0 ./script/train_pitts30k_partial.sbatch 
+sbatch --export=ALL,PRETRAIN=bt,SEED=1 ./script/train_pitts30k_partial.sbatch 
+sbatch --export=ALL,PRETRAIN=bt,SEED=2 ./script/train_pitts30k_partial.sbatch 
 
