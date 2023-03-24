94a204680a5603fc2d6f8a5aa615e3cb4ad6011e
Jiuhong Xiao <jx1190@log-2.hpc.nyu.edu>
2023-03-24 12:04:35 -0400

    change eval part

--- util.py
+++ util.py
@@ -45,6 +45,27 @@ def resume_model(args, model):
     model.load_state_dict(state_dict)
     return model
 
+def resume_model(args, model, suffix=None):
+    checkpoint = torch.load(args.resume, map_location=args.device)
+    if f"model_{suffix}_state_dict" in checkpoint:
+        state_dict = checkpoint[f"model_{suffix}_state_dict"]
+    else:
+        # The pre-trained models that we provide in the README do not have 'state_dict' in the keys as
+        # the checkpoint is directly the state dict
+        state_dict = checkpoint
+    # if the model contains the prefix "module" which is appendend by
+    # DataParallel, remove it to avoid errors when loading dict
+    if list(state_dict.keys())[0].startswith("module"):
+        state_dict = OrderedDict(
+            {k.replace("module.", ""): v for (k, v) in state_dict.items()}
+        )
+    model.load_state_dict(state_dict)
+    return model
+
+def resume_model_ssl(args, model):
+    model.backbone = resume_model(args, model, suffix="backbone")
+    model.aggregation = resume_model(args, model, suffix="aggregation")
+    return model
 
 def resume_train(args, model, optimizer=None, strict=False):
     """Load model, optimizer, and other training parameters"""
@@ -72,27 +93,6 @@ def resume_train_ssl(args, model, optimizer=None, strict=False):
     checkpoint = torch.load(args.resume)
     start_epoch_num = checkpoint["epoch_num"]
     model.ssl_model.load_state_dict(checkpoint["model_ssl_state_dict"], strict=strict)
-    if optimizer:
-        optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
-    best_r5 = checkpoint["best_r5"]
-    not_improved_num = checkpoint["not_improved_num"]
-    logging.debug(
-        f"Loaded checkpoint: start_epoch_num = {start_epoch_num}, "
-        f"current_best_R@5 = {best_r5:.1f}"
-    )
-    if args.resume.endswith("last_model.pth"):  # Copy best model to current save_dir
-        shutil.copy(
-            args.resume.replace("last_model.pth", "best_model.pth"), args.save_dir
-        )
-    return model, optimizer, best_r5, start_epoch_num, not_improved_num
-
-def resume_train_ssl_only_eval(args, model, optimizer=None, strict=False):
-    """Load model, optimizer, and other training parameters"""
-    logging.debug(f"Loading checkpoint: {args.resume}")
-    checkpoint = torch.load(args.resume)
-    start_epoch_num = checkpoint["epoch_num"]
-    model.backbone.load_state_dict(checkpoint["model_backbone_state_dict"], strict=strict)
-    model.aggregation.load_state_dict(checkpoint["model_aggregation_state_dict"], strict=strict)
     if optimizer:
         optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
     best_r5 = checkpoint["best_r5"]
