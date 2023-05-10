d80b8d52c6627edeb290e33d7963d112e7a9b33a
Jiuhong Xiao <jx1190@log-2.hpc.nyu.edu>
2023-05-09 21:20:21 -0400

    script final

--- train_msls_vicreg_final_sbatch.sh
+++ train_msls_vicreg_final_sbatch.sh
@@ -7,9 +7,18 @@
 # sbatch --export=ALL,SSL=bt,SEED=0,LR=1e-5,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_vicreg_neg1.sbatch
 # sbatch --export=ALL,SSL=vicreg,SEED=0,LR=1e-5,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_vicreg_neg1.sbatch
 
-sbatch --export=ALL,SSL=bt,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=3 ./script/train_msls_ssl_long_vicreg_neg1.sbatch
-sbatch --export=ALL,SSL=vicreg,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=3 ./script/train_msls_ssl_long_vicreg_neg1.sbatch
-sbatch --export=ALL,SSL=bt,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_vicreg_neg1.sbatch
-sbatch --export=ALL,SSL=vicreg,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_vicreg_neg1.sbatch
-sbatch --export=ALL,SSL=bt,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=1 ./script/train_msls_ssl_long_vicreg_neg1.sbatch
-sbatch --export=ALL,SSL=vicreg,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=1 ./script/train_msls_ssl_long_vicreg_neg1.sbatch
+# sbatch --export=ALL,SSL=bt,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=3 ./script/train_msls_ssl_long_vicreg_neg1.sbatch
+# sbatch --export=ALL,SSL=vicreg,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=3 ./script/train_msls_ssl_long_vicreg_neg1.sbatch
+# sbatch --export=ALL,SSL=bt,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=1 ./script/train_msls_ssl_long_vicreg_neg1.sbatch
+# sbatch --export=ALL,SSL=vicreg,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=1 ./script/train_msls_ssl_long_vicreg_neg1.sbatch
+
+# sbatch --export=ALL,SSL=bt,SEED=0,LR=1e-4,BATCH=64,PROJ=1024,LAY=2 ./script/train_msls_ssl_long_vicreg_neg1.sbatch
+# sbatch --export=ALL,SSL=vicreg,SEED=0,LR=1e-4,BATCH=64,PROJ=1024,LAY=2 ./script/train_msls_ssl_long_vicreg_neg1.sbatch
+# sbatch --export=ALL,SSL=bt,SEED=0,LR=1e-4,BATCH=64,PROJ=4096,LAY=2 ./script/train_msls_ssl_long_vicreg_neg1.sbatch
+# sbatch --export=ALL,SSL=vicreg,SEED=0,LR=1e-4,BATCH=64,PROJ=4096,LAY=2 ./script/train_msls_ssl_long_vicreg_neg1.sbatch
+
+# sbatch --export=ALL,SSL=bt,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_vicreg.sbatch
+# sbatch --export=ALL,SSL=vicreg,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_vicreg.sbatch
+
+sbatch --export=ALL,SSL=bt,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_vicreg_neg1_128.sbatch
+sbatch --export=ALL,SSL=vicreg,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_vicreg_neg1_128.sbatch
