c812f3ea0e1b2f40aef5cd259c72a68a312fc51f
Jiuhong Xiao <jx1190@log-2.hpc.nyu.edu>
2023-05-17 11:28:58 -0400

    change env

--- train_msls_byol_final_sbatch.sh
+++ train_msls_byol_final_sbatch.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
-# PRETRAIN
-# sbatch --export=ALL,SSL=byol,SEED=0,LR=1e-3,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_byol_neg1.sbatch
-# sbatch --export=ALL,SSL=simsiam,SEED=0,LR=1e-3,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_byol_neg1.sbatch
-# sbatch --export=ALL,SSL=byol,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_byol_neg1.sbatch
-# sbatch --export=ALL,SSL=simsiam,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_byol_neg1.sbatch
-# sbatch --export=ALL,SSL=byol,SEED=0,LR=1e-5,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_byol_neg1.sbatch
-# sbatch --export=ALL,SSL=simsiam,SEED=0,LR=1e-5,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_byol_neg1.sbatch
+sbatch --export=ALL,SSL=byol,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2,NEG=1.0 ./script/train_msls_ssl_long_byol_neg.sbatch
+sbatch --export=ALL,SSL=simsiam,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2,NEG=1.0 ./script/train_msls_ssl_long_byol_neg.sbatch
+sbatch --export=ALL,SSL=byol,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2,NEG=0.5 ./script/train_msls_ssl_long_byol_neg.sbatch
+sbatch --export=ALL,SSL=simsiam,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2,NEG=0.5 ./script/train_msls_ssl_long_byol_neg.sbatch
+sbatch --export=ALL,SSL=byol,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2,NEG=0.25 ./script/train_msls_ssl_long_byol_neg.sbatch
+sbatch --export=ALL,SSL=simsiam,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2,NEG=0.25 ./script/train_msls_ssl_long_byol_neg.sbatch
+sbatch --export=ALL,SSL=byol,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2,NEG=0.0 ./script/train_msls_ssl_long_byol_neg.sbatch
+sbatch --export=ALL,SSL=simsiam,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2,NEG=0.0 ./script/train_msls_ssl_long_byol_neg.sbatch
 
-# sbatch --export=ALL,SSL=byol,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=3 ./script/train_msls_ssl_long_byol_neg1.sbatch
-# sbatch --export=ALL,SSL=simsiam,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=3 ./script/train_msls_ssl_long_byol_neg1.sbatch
-# sbatch --export=ALL,SSL=byol,SEED=1,LR=1e-4,BATCH=64,PROJ=2048,LAY=1 ./script/train_msls_ssl_long_byol_neg1.sbatch
-# sbatch --export=ALL,SSL=simsiam,SEED=1,LR=1e-4,BATCH=64,PROJ=2048,LAY=1 ./script/train_msls_ssl_long_byol_neg1.sbatch
+sbatch --export=ALL,SSL=byol,SEED=0,LR=1e-3,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_byol_neg025.sbatch
+sbatch --export=ALL,SSL=simsiam,SEED=0,LR=1e-3,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_byol_neg025.sbatch
+sbatch --export=ALL,SSL=byol,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_byol_neg025.sbatch
+sbatch --export=ALL,SSL=simsiam,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_byol_neg025.sbatch
+sbatch --export=ALL,SSL=byol,SEED=0,LR=1e-5,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_byol_neg025.sbatch
+sbatch --export=ALL,SSL=simsiam,SEED=0,LR=1e-5,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_byol_neg025.sbatch
 
-# sbatch --export=ALL,SSL=byol,SEED=0,LR=1e-4,BATCH=64,PROJ=1024,LAY=2 ./script/train_msls_ssl_long_byol_neg1.sbatch
-# sbatch --export=ALL,SSL=simsiam,SEED=0,LR=1e-4,BATCH=64,PROJ=1024,LAY=2 ./script/train_msls_ssl_long_byol_neg1.sbatch
-# sbatch --export=ALL,SSL=byol,SEED=0,LR=1e-4,BATCH=64,PROJ=4096,LAY=2 ./script/train_msls_ssl_long_byol_neg1.sbatch
-# sbatch --export=ALL,SSL=simsiam,SEED=0,LR=1e-4,BATCH=64,PROJ=4096,LAY=2 ./script/train_msls_ssl_long_byol_neg1.sbatch
+sbatch --export=ALL,SSL=byol,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=3 ./script/train_msls_ssl_long_byol_neg025.sbatch
+sbatch --export=ALL,SSL=simsiam,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=3 ./script/train_msls_ssl_long_byol_neg025.sbatch
+sbatch --export=ALL,SSL=byol,SEED=1,LR=1e-4,BATCH=64,PROJ=2048,LAY=1 ./script/train_msls_ssl_long_byol_neg025.sbatch
+sbatch --export=ALL,SSL=simsiam,SEED=1,LR=1e-4,BATCH=64,PROJ=2048,LAY=1 ./script/train_msls_ssl_long_byol_neg025.sbatch
 
-# sbatch --export=ALL,SSL=byol,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_byol.sbatch
-# sbatch --export=ALL,SSL=simsiam,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_byol.sbatch
+sbatch --export=ALL,SSL=byol,SEED=0,LR=1e-4,BATCH=64,PROJ=1024,LAY=2 ./script/train_msls_ssl_long_byol_neg025.sbatch
+sbatch --export=ALL,SSL=simsiam,SEED=0,LR=1e-4,BATCH=64,PROJ=1024,LAY=2 ./script/train_msls_ssl_long_byol_neg025.sbatch
+sbatch --export=ALL,SSL=byol,SEED=0,LR=1e-4,BATCH=64,PROJ=4096,LAY=2 ./script/train_msls_ssl_long_byol_neg025.sbatch
+sbatch --export=ALL,SSL=simsiam,SEED=0,LR=1e-4,BATCH=64,PROJ=4096,LAY=2 ./script/train_msls_ssl_long_byol_neg025.sbatch
 
-sbatch --export=ALL,SSL=byol,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_byol_neg1_128.sbatch
-sbatch --export=ALL,SSL=simsiam,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_byol_neg1_128.sbatch
+sbatch --export=ALL,SSL=byol,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_byol_neg025_128.sbatch
+sbatch --export=ALL,SSL=simsiam,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_byol_neg025_128.sbatch
+
+sbatch --export=ALL,SSL=byol,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_byol_neg025_256.sbatch
+sbatch --export=ALL,SSL=simsiam,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=2 ./script/train_msls_ssl_long_byol_neg025_256.sbatch
--- train_msls_byol_sbatch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# PRETRAIN
-sbatch --export=ALL,SSL=byol,SEED=0 ./script/train_msls_ssl.sbatch 
-sbatch --export=ALL,SSL=simsiam,SEED=0 ./script/train_msls_ssl.sbatch 
