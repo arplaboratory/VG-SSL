7c423d2f559142c2ab5b3a1639fd2b6dd5345900
Jiuhong Xiao <jx1190@log-3.hpc.nyu.edu>
2023-05-19 14:41:11 -0400

    script for simclr

--- train_msls_simclr_final_sbatch.sh
+++ train_msls_simclr_final_sbatch.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sbatch --export=ALL,SSL=simclr,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=1,NEG=1.0 ./script/train_msls_ssl_long_simclr_neg.sbatch
-sbatch --export=ALL,SSL=mocov2,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=1,NEG=1.0 ./script/train_msls_ssl_long_simclr_neg.sbatch
-sbatch --export=ALL,SSL=simclr,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=1,NEG=0.5 ./script/train_msls_ssl_long_simclr_neg.sbatch
-sbatch --export=ALL,SSL=mocov2,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=1,NEG=0.5 ./script/train_msls_ssl_long_simclr_neg.sbatch
-sbatch --export=ALL,SSL=simclr,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=1,NEG=0.25 ./script/train_msls_ssl_long_simclr_neg.sbatch
-sbatch --export=ALL,SSL=mocov2,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=1,NEG=0.25 ./script/train_msls_ssl_long_simclr_neg.sbatch
-sbatch --export=ALL,SSL=simclr,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=1,NEG=0.0 ./script/train_msls_ssl_long_simclr_neg.sbatch
-sbatch --export=ALL,SSL=mocov2,SEED=0,LR=1e-4,BATCH=64,PROJ=2048,LAY=1,NEG=0.0 ./script/train_msls_ssl_long_simclr_neg.sbatch
+sbatch --export=ALL,SSL=simclr,SEED=0,LR=1e-5,BATCH=64,PROJ=2048,LAY=1,NEG=1.0 ./script/train_msls_ssl_long_simclr_neg.sbatch
+sbatch --export=ALL,SSL=mocov2,SEED=0,LR=1e-5,BATCH=64,PROJ=2048,LAY=1,NEG=1.0 ./script/train_msls_ssl_long_simclr_neg.sbatch
+sbatch --export=ALL,SSL=simclr,SEED=0,LR=1e-5,BATCH=64,PROJ=2048,LAY=1,NEG=0.5 ./script/train_msls_ssl_long_simclr_neg.sbatch
+sbatch --export=ALL,SSL=mocov2,SEED=0,LR=1e-5,BATCH=64,PROJ=2048,LAY=1,NEG=0.5 ./script/train_msls_ssl_long_simclr_neg.sbatch
+sbatch --export=ALL,SSL=simclr,SEED=0,LR=1e-5,BATCH=64,PROJ=2048,LAY=1,NEG=0.25 ./script/train_msls_ssl_long_simclr_neg.sbatch
+sbatch --export=ALL,SSL=mocov2,SEED=0,LR=1e-5,BATCH=64,PROJ=2048,LAY=1,NEG=0.25 ./script/train_msls_ssl_long_simclr_neg.sbatch
+sbatch --export=ALL,SSL=simclr,SEED=0,LR=1e-5,BATCH=64,PROJ=2048,LAY=1,NEG=0.0 ./script/train_msls_ssl_long_simclr_neg.sbatch
+sbatch --export=ALL,SSL=mocov2,SEED=0,LR=1e-5,BATCH=64,PROJ=2048,LAY=1,NEG=0.0 ./script/train_msls_ssl_long_simclr_neg.sbatch
 
 # sbatch --export=ALL,SSL=simclr,SEED=0,LR=1e-3,BATCH=64,PROJ=2048,LAY=1 ./script/train_msls_ssl_long_simclr_neg1.sbatch
 # sbatch --export=ALL,SSL=mocov2,SEED=0,LR=1e-3,BATCH=64,PROJ=2048,LAY=1 ./script/train_msls_ssl_long_simclr_neg1.sbatch
