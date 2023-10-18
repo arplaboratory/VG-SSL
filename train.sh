106995ceeb5670c3bca49c09b0fb331dd2fd087a
Jiuhong Xiao <jx1190@log-2.hpc.nyu.edu>
2023-10-17 23:53:19 -0400

    train r2former

--- train.sh
+++ train.sh
@@ -1,24 +1,18 @@
 # Global retrieval
  # ResNet50conv5 + GEM
 # sbatch script/train_msls_partial_50.sbatch
- # ResNet101conv5 + GEM
-# sbatch script/train_msls_partial_101.sbatch
- # ResNet50conv5 + GEM + unfreeze
-# sbatch script/train_msls_partial_50_unfreeze.sbatch
- # ResNet101conv5 + GEM + unfreeze
-# sbatch script/train_msls_partial_101_unfreeze.sbatch
- # Deitbase + GEM
-# sbatch script/train_msls_partial_deit_b.sbatch
  # Deit + GEM
 # sbatch script/train_msls_partial_deit.sbatch
 
 
 # Reranking
  # ResNet50conv5 + GEM + unfreeze
-sbatch script/train_msls_rerank_50_unfreeze.sbatch
- # ResNet101conv5 + GEM + unfreeze
-sbatch script/train_msls_rerank_101_unfreeze.sbatch
- # Deitbase + GEM
-sbatch script/train_msls_rerank_deit_b.sbatch
+# sbatch script/train_msls_rerank_50_unfreeze.sbatch
  # Deit + GEM
-sbatch script/train_msls_rerank_deit.sbatch
+# sbatch script/train_msls_rerank_deit.sbatch
+
+# Finetuning
+ # ResNet50conv5 + GEM + unfreeze
+# sbatch script/train_msls_finetune_50_unfreeze.sbatch
+ # Deit + GEM
+# sbatch script/train_msls_finetune_deit.sbatch
