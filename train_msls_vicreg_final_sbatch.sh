6a1fcb24c178bd869eb0662c3794af927227eacd
Jiuhong Xiao <jx1190@log-2.hpc.nyu.edu>
2023-10-17 23:52:02 -0400

    train scripts

--- train_msls_vicreg_final_sbatch.sh
+++ train_msls_vicreg_final_sbatch.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
-# None vs random vs partial
-sbatch --export=ALL,SSL=vicreg,LR=1e-5,BATCH=64,PROJ=2048,LAY=2,NEG=0,FC=1024,NEGQ=0 ./script/train_msls_ssl_long_vicreg_neg_random_pair.sbatch
-sbatch --export=ALL,SSL=bt,LR=1e-5,BATCH=64,PROJ=2048,LAY=2,NEG=0,FC=1024,NEGQ=0 ./script/train_msls_ssl_long_vicreg_neg_random_pair.sbatch
-sbatch --export=ALL,SSL=vicreg,LR=1e-5,BATCH=64,PROJ=2048,LAY=2,NEG=1000,FC=1024,NEGQ=10 ./script/train_msls_ssl_long_vicreg_neg_random_pair.sbatch
-sbatch --export=ALL,SSL=bt,LR=1e-5,BATCH=64,PROJ=2048,LAY=2,NEG=1000,FC=1024,NEGQ=10 ./script/train_msls_ssl_long_vicreg_neg_random_pair.sbatch
-sbatch --export=ALL,SSL=vicreg,LR=1e-5,BATCH=64,PROJ=2048,LAY=2,NEG=1000,FC=1024,NEGQ=10 ./script/train_msls_ssl_long_vicreg_neg_partial_pair.sbatch
-sbatch --export=ALL,SSL=bt,LR=1e-5,BATCH=64,PROJ=2048,LAY=2,NEG=1000,FC=1024,NEGQ=10 ./script/train_msls_ssl_long_vicreg_neg_partial_pair.sbatch
-sbatch --export=ALL,SSL=vicreg,LR=1e-5,BATCH=64,PROJ=2048,LAY=2,NEG=1000,FC=1024,NEGQ=1 ./script/train_msls_ssl_long_vicreg_neg_partial_pair.sbatch
-sbatch --export=ALL,SSL=bt,LR=1e-5,BATCH=64,PROJ=2048,LAY=2,NEG=1000,FC=1024,NEGQ=1 ./script/train_msls_ssl_long_vicreg_neg_partial_pair.sbatch
+# Random vs partial
+# sbatch --export=ALL,SSL=vicreg,LR=1e-5,BATCH=64,PROJ=2048,LAY=2,NEG=0,FC=1024,NEGQ=0 ./script/train_msls_ssl_long_vicreg_neg_random_pair.sbatch
+# sbatch --export=ALL,SSL=bt,LR=1e-5,BATCH=64,PROJ=2048,LAY=2,NEG=0,FC=1024,NEGQ=0 ./script/train_msls_ssl_long_vicreg_neg_random_pair.sbatch
+# sbatch --export=ALL,SSL=vicreg,LR=1e-5,BATCH=64,PROJ=2048,LAY=2,NEG=1000,FC=1024,NEGQ=1 ./script/train_msls_ssl_long_vicreg_neg_partial_pair_aug.sbatch
+# sbatch --export=ALL,SSL=bt,LR=1e-5,BATCH=64,PROJ=2048,LAY=2,NEG=1000,FC=1024,NEGQ=1 ./script/train_msls_ssl_long_vicreg_neg_partial_pair_aug.sbatch
 
-# # Different number of layers
-sbatch --export=ALL,SSL=vicreg,LR=1e-5,BATCH=64,PROJ=2048,LAY=0,NEG=1000,FC=1024,NEGQ=10 ./script/train_msls_ssl_long_vicreg_neg_partial_pair.sbatch
-sbatch --export=ALL,SSL=bt,LR=1e-5,BATCH=64,PROJ=2048,LAY=0,NEG=1000,FC=1024,NEGQ=10 ./script/train_msls_ssl_long_vicreg_neg_partial_pair.sbatch
-sbatch --export=ALL,SSL=vicreg,LR=1e-5,BATCH=64,PROJ=2048,LAY=1,NEG=1000,FC=1024,NEGQ=10 ./script/train_msls_ssl_long_vicreg_neg_partial_pair.sbatch
-sbatch --export=ALL,SSL=bt,LR=1e-5,BATCH=64,PROJ=2048,LAY=1,NEG=1000,FC=1024,NEGQ=10 ./script/train_msls_ssl_long_vicreg_neg_partial_pair.sbatch
-sbatch --export=ALL,SSL=vicreg,LR=1e-5,BATCH=64,PROJ=2048,LAY=3,NEG=1000,FC=1024,NEGQ=10 ./script/train_msls_ssl_long_vicreg_neg_partial_pair.sbatch
-sbatch --export=ALL,SSL=bt,LR=1e-5,BATCH=64,PROJ=2048,LAY=3,NEG=1000,FC=1024,NEGQ=10 ./script/train_msls_ssl_long_vicreg_neg_partial_pair.sbatch
+# sbatch --export=ALL,SSL=vicreg,LR=1e-5,BATCH=64,PROJ=4096,LAY=2,NEG=1000,FC=1024,NEGQ=1 ./script/train_msls_ssl_long_vicreg_neg_partial_pair_aug_large.sbatch
+# sbatch --export=ALL,SSL=bt,LR=1e-5,BATCH=64,PROJ=4096,LAY=2,NEG=1000,FC=1024,NEGQ=1 ./script/train_msls_ssl_long_vicreg_neg_partial_pair_aug_large.sbatch
 
-# # Different number of proj dim
-sbatch --export=ALL,SSL=vicreg,LR=1e-5,BATCH=64,PROJ=4096,LAY=2,NEG=1000,FC=1024,NEGQ=10 ./script/train_msls_ssl_long_vicreg_neg_partial_pair.sbatch
-sbatch --export=ALL,SSL=bt,LR=1e-5,BATCH=64,PROJ=4096,LAY=2,NEG=1000,FC=1024,NEGQ=10 ./script/train_msls_ssl_long_vicreg_neg_partial_pair.sbatch
-sbatch --export=ALL,SSL=vicreg,LR=1e-5,BATCH=64,PROJ=1024,LAY=2,NEG=1000,FC=1024,NEGQ=10 ./script/train_msls_ssl_long_vicreg_neg_partial_pair.sbatch
-sbatch --export=ALL,SSL=bt,LR=1e-5,BATCH=64,PROJ=1024,LAY=2,NEG=1000,FC=1024,NEGQ=10 ./script/train_msls_ssl_long_vicreg_neg_partial_pair.sbatch
+# Different number of layers
+# sbatch --export=ALL,SSL=vicreg,LR=1e-5,BATCH=64,PROJ=2048,LAY=0,NEG=1000,FC=1024,NEGQ=1 ./script/train_msls_ssl_long_vicreg_neg_partial_pair_aug.sbatch
+# sbatch --export=ALL,SSL=bt,LR=1e-5,BATCH=64,PROJ=2048,LAY=0,NEG=1000,FC=1024,NEGQ=1 ./script/train_msls_ssl_long_vicreg_neg_partial_pair_aug.sbatch
+# sbatch --export=ALL,SSL=vicreg,LR=1e-5,BATCH=64,PROJ=2048,LAY=1,NEG=1000,FC=1024,NEGQ=1 ./script/train_msls_ssl_long_vicreg_neg_partial_pair_aug.sbatch
+# sbatch --export=ALL,SSL=bt,LR=1e-5,BATCH=64,PROJ=2048,LAY=1,NEG=1000,FC=1024,NEGQ=1 ./script/train_msls_ssl_long_vicreg_neg_partial_pair_aug.sbatch
+# sbatch --export=ALL,SSL=vicreg,LR=1e-5,BATCH=64,PROJ=2048,LAY=3,NEG=1000,FC=1024,NEGQ=1 ./script/train_msls_ssl_long_vicreg_neg_partial_pair_aug.sbatch
+# sbatch --export=ALL,SSL=bt,LR=1e-5,BATCH=64,PROJ=2048,LAY=3,NEG=1000,FC=1024,NEGQ=1 ./script/train_msls_ssl_long_vicreg_neg_partial_pair_aug.sbatch
 
-# # Different number of fc dim
-sbatch --export=ALL,SSL=vicreg,LR=1e-5,BATCH=64,PROJ=2048,LAY=2,NEG=1000,FC=512,NEGQ=10 ./script/train_msls_ssl_long_vicreg_neg_partial_pair.sbatch
-sbatch --export=ALL,SSL=bt,LR=1e-5,BATCH=64,PROJ=2048,LAY=2,NEG=1000,FC=512,NEGQ=10 ./script/train_msls_ssl_long_vicreg_neg_partial_pair.sbatch
-sbatch --export=ALL,SSL=vicreg,LR=1e-5,BATCH=64,PROJ=2048,LAY=2,NEG=1000,FC=2048,NEGQ=10 ./script/train_msls_ssl_long_vicreg_neg_partial_pair.sbatch
-sbatch --export=ALL,SSL=bt,LR=1e-5,BATCH=64,PROJ=2048,LAY=2,NEG=1000,FC=2048,NEGQ=10 ./script/train_msls_ssl_long_vicreg_neg_partial_pair.sbatch
+# Different number of proj dim
+# sbatch --export=ALL,SSL=vicreg,LR=1e-5,BATCH=64,PROJ=4096,LAY=2,NEG=1000,FC=1024,NEGQ=1 ./script/train_msls_ssl_long_vicreg_neg_partial_pair_aug.sbatch
+# sbatch --export=ALL,SSL=bt,LR=1e-5,BATCH=64,PROJ=4096,LAY=2,NEG=1000,FC=1024,NEGQ=1 ./script/train_msls_ssl_long_vicreg_neg_partial_pair_aug.sbatch
+# sbatch --export=ALL,SSL=vicreg,LR=1e-5,BATCH=64,PROJ=1024,LAY=2,NEG=1000,FC=1024,NEGQ=1 ./script/train_msls_ssl_long_vicreg_neg_partial_pair_aug.sbatch
+# sbatch --export=ALL,SSL=bt,LR=1e-5,BATCH=64,PROJ=1024,LAY=2,NEG=1000,FC=1024,NEGQ=1 ./script/train_msls_ssl_long_vicreg_neg_partial_pair_aug.sbatch
+
+# Different number of fc dim
+# sbatch --export=ALL,SSL=vicreg,LR=1e-5,BATCH=64,PROJ=2048,LAY=2,NEG=1000,FC=512,NEGQ=1 ./script/train_msls_ssl_long_vicreg_neg_partial_pair_aug.sbatch
+# sbatch --export=ALL,SSL=bt,LR=1e-5,BATCH=64,PROJ=2048,LAY=2,NEG=1000,FC=512,NEGQ=1 ./script/train_msls_ssl_long_vicreg_neg_partial_pair_aug.sbatch
+# sbatch --export=ALL,SSL=vicreg,LR=1e-5,BATCH=64,PROJ=2048,LAY=2,NEG=1000,FC=2048,NEGQ=1 ./script/train_msls_ssl_long_vicreg_neg_partial_pair_aug.sbatch
+# sbatch --export=ALL,SSL=bt,LR=1e-5,BATCH=64,PROJ=2048,LAY=2,NEG=1000,FC=2048,NEGQ=1 ./script/train_msls_ssl_long_vicreg_neg_partial_pair_aug.sbatch
